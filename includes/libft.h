8b6a0b8bb2607d4f62f3a0e5e3e471f7cce9da84
Robin Dehouck <robindehouck@Robins-iMac.local>
2021-11-11 23:37:52 +0100

    11 nov 2021 final commit

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: robindehouck <robindehouck@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:59:46 by rdehouck          #+#    #+#             */
-/*   Updated: 2021/11/11 23:02:03 by robindehouc      ###   ########.fr       */
+/*   Updated: 2021/11/11 23:37:48 by robindehouc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ size_t		ft_strlen(const char *str);
 void		*ft_memset(void *buf, int c, int len);
 void		ft_bzero(void *string, int byte_length);
 void		*ft_memcpy(void *dest, void *source, unsigned int bit_size);
-void		*ft_memmove(void *dest, void *source, unsigned int bit_size);
+void		*ft_memmove(void *dest, const void *src, size_t len)
 size_t		ft_strlcpy(char *dest, char *source, size_t length);
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
 int			ft_toupper(int c);
